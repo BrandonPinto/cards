78c34cc76076d750d6a8b96d729957843e8d88fd
Brandon <b.pintony@gmail.com>
2024-02-14 22:19:48 -0500

    Added a deck of cards along with the ability to manipulate the array -- server sided.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,42 @@
 const express = require('express')
 const cors = require('cors')
+const {freeze, drop, take, shuffle} = require('ez-read')
+const initializeDeck = require('./deck-init')
+
+class Deck 
+{
+    suits = freeze(require('./suits-init'))
+    ranks = freeze(['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'])
+    deck = shuffle(initializeDeck(this.suits, this.ranks))
+
+    show() 
+    {
+        console.log(this.deck)
+    }
+    shuffleDeck() 
+    {
+        this.deck = shuffle(this.deck)
+    }
+    discard(num) 
+    {
+        this.deck = drop(num, this.deck)
+    }
+    deal(num) 
+    {
+        const hand = take(num, this.deck)
+        this.deck = drop(num, this.deck)
+        return hand
+    }
+}
+const deck = new Deck()
+
+deck.show()
+const myHand = deck.deal(8)
+deck.show()
+console.log(myHand)
+
 const app = express()
+
 app.use(cors())
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] })
---
+++ server/suits-init.js
@@ -0,0 +1,8 @@
+const emoji = require('node-emoji')
+
+const spades = emoji.get('spades')
+const hearts = emoji.get('hearts')
+const clubs = emoji.get('clubs')
+const diamonds = emoji.get('diamonds')
+
+module.exports = [spades, hearts, clubs, diamonds]
